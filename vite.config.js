95ab9c20b0f09ba3b697d9389aa2368d573f6649
Azadarx <syedazadarhussayn@gmail.com>
2025-04-22 22:13:45 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  // server: {
-  //   proxy: {
-  //     '/api': 'https://inspiringshereen-backend.onrender.com',
-  //     historyApiFallback: true,
-  //   },
-  // },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // your local backend
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+      '/api': 'https://asta-backend-o8um.onrender.com',
+      historyApiFallback: true,
+    },
+  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000', // your local backend
+  //       changeOrigin: true,
+  //       secure: false
+  //     }
+  //   }
+  // }
 })
