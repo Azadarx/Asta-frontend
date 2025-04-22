28fa42ee352c4c771d0a8940edf3a79f8ddccc67
Azadarx <syedazadarhussayn@gmail.com>
2025-04-22 23:12:32 +0530

    Fix: path case or missing file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
-  
+
 export const firebaseConfig = {
   apiKey: "AIzaSyCk3nSg0O3uxkwyHFJCfcN9SIu17z3W1Ys",
   authDomain: "phonicslms.firebaseapp.com",
@@ -19,5 +19,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 const storage = getStorage(app);
-
-export { firebaseConfig, auth, database, storage };
+export { auth, database, storage };
